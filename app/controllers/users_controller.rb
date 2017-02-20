2a000587bca47082b4caaae76d5eecbf67730ef9
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-20 12:35:47 -0500

    devise changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,27 @@ class UsersController < ApplicationController
       @users = User.all
     end
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users, controllers: {
-    confirmations: 'users/confirmations',
-    passwords: 'users/passwords',
-    registrations: 'users/registrations',
-    sessions: 'users/sessions',
-    unlocks: 'users/unlocks'
-  }
-
-  devise_for :admins, controllers: {
-    confirmations: 'admins/confirmations',
-    passwords: 'admins/passwords',
-    registrations: 'admins/registrations',
-    sessions: 'admins/sessions',
-    unlocks: 'admins/unlocks'
-  }
+  devise_for :users
+  devise_for :admins
 
   resources :home, only: [:index]
   resources :users, only: [:index, :show]
