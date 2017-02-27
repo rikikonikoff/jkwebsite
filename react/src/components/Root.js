9096fce97121b11b3ecb69f50e96bccd4780568c
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-27 10:44:37 -0500

    pass in current user to react

--- react/src/components/Root.js
+++ react/src/components/Root.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { Router } from 'react-router';
 
-const Root = ({browserHistory, routes}) => {
+const Root = ({browserHistory, routes, current_user, current_admin}) => {
   return(
-    <Router history={browserHistory} routes={routes} />
+    <Router
+    history={browserHistory}
+    routes={routes}
+    current_user={current_user}
+    current_admin={current_admin}
+    />
   );
 };
 
