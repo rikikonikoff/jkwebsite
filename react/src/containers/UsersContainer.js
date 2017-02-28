987ccf79833a8857f01105cd2b87532f6de1aa9d
Riki Konikoff <riki.konikoff@gmail.com>
2017-02-27 20:50:29 -0500

    bootstrap

--- react/src/containers/UsersContainer.js
+++ react/src/containers/UsersContainer.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router';
 import UsersIndex from '../components/UsersIndex';
-import UserSignUp from '../components/UserSignUp';
-import UserSignIn from '../components/UserSignIn';
 
 const UsersContainer = (props) => {
   return(
     <div>
-      <UserSignUp />
-      <UserSignIn />
-      <UsersIndex />
+      {props.current_admin &&
+        <UsersIndex />}
     </div>
   );
 };
