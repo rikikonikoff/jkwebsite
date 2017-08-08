e822e783211e6714b6f8e38395ddd8ace05a697a
Riki Konikoff <riki.konikoff@gmail.com>
2017-08-08 10:28:36 -0400

    fixup wheresjacob controller

--- app/controllers/api/v1/wheres_jacobs_controller.rb
+++ app/controllers/api/v1/wheres_jacobs_controller.rb
@@ -3,22 +3,31 @@ class Api::V1::WheresJacobsController < ApplicationController
     if: Proc.new { |c| c.request.format == 'application/json' }
 
   def index
-    @where = WheresJacob.last
+    @where = WheresJacob.all.sort_by(&:updated_at).last
     render json: @where
   end
 
   def create
-    @where = WheresJacob.new(where_params)
-    if @where.save
-      flash[:notice] = "you updated your location"
-      redirect_to "/wheresjacob"
+    if @existing = WheresJacob.find_by(location: where_params[:location])
+      @existing.update_attributes!
+      redirect
     else
-      flash[:notice] = @where.errors.full_messages.to_sentence
-      render "wheres_jacobs/index"
+      @where = WheresJacob.new(where_params)
+      if @where.save
+        redirect
+      else
+        flash[:notice] = @where.errors.full_messages.to_sentence
+        render "wheres_jacobs/index"
+      end
     end
   end
 
   private
+
+  def redirect
+    flash[:notice] = "you updated your location"
+    redirect_to "/wheresjacob"
+  end
 
   def where_params
     params.require(:wheres_jacob).permit(:location)
